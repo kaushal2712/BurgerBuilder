8a1b9d82d96e141a27da038c321f2bc836641cca
kajha <kajha@adobe.com>
2018-12-30 16:28:11 +0530

    burger component, burger ingredient
    burger component, burger ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 
 import Aux from "../../hoc/Aux";
 
+import Burger from "../../components/Burger/Burger";
+
 export default class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     );
