161e36aa4a639e48495b7886697e97cc8a82d8a1
kajha <kajha@adobe.com>
2019-01-04 01:10:11 +0530

    Sending post request
    Sending post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -46,7 +47,26 @@ export default class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
-  purchaseContinueHandler = () => {};
+  purchaseContinueHandler = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "kaushal",
+        address: {
+          street: "test street1",
+          zipCode: "213243",
+          country: "Germany"
+        },
+        email: "test@test.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("./orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
 
   addIngredientHandler = type => {
     const oldCount = this.state.ingredients[type];
